1eb7c40571378245641d3e15df7ff112f0dea14d
REMitchell <Ryan.E.Mitchell@gmail.com>
2021-10-20 18:36:14 -0400

    Tweaks to starting state, added 01_03

---
+++ 01_03/friendsbook/post/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.deletion import CASCADE
+
+# Create your models here.
+class Post(models.Model):
+    author = models.ForeignKey(User, null=True, blank=True, on_delete=CASCADE)
+    content = models.CharField(max_length=140)
+    created_on = models.DateTimeField('date created', auto_now_add=True)
+
+    def __str__(self):
+        return '{}: {}'.format(self.author, self.content[0:20])
---
+++ 01_03/friendsbook/profilepage/views.py
@@ -0,0 +1,25 @@
+from django.http.response import HttpResponse, HttpResponseForbidden
+from django.shortcuts import render
+from django.template import loader
+from django.shortcuts import render, get_object_or_404
+from django.forms.models import model_to_dict
+
+from .models import ProfilePage
+from django.apps import apps
+
+import datetime
+
+
+def profile_detail(request, profilepage_id):
+    template = loader.get_template('profilepage.html')
+    profile = ProfilePage.objects.get(id=profilepage_id)
+    Post = apps.get_model('post', 'Post')
+    print('AUTHOR IS:')
+    print(profile.user_account.user)
+    posts = Post.objects.filter(author=profile.user_account.user)
+    print('POSTS ARE:')
+    print(posts)
+    own_profile = request.user and (request.user.id == profile.user_account.user.id)
+    return HttpResponse(template.render({'profile': profile, 'posts': posts, 'own_profile': own_profile}, request))
+
+
